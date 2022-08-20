bc8e8625c1cdf101871933c6b894c27444385b59
harvis <getharisree@outlook.com>
2022-08-20 13:42:36 +0530

    smoothening the img

--- conversion to grey scale.py
+++ conversion to grey scale.py
@@ -4,7 +4,7 @@ import numpy as np
 img = cv2.imread("test_image.jpg")
 imgcpy = np.copy(img)
 img_gray = cv2.cvtColor(imgcpy,cv2.COLOR_BGR2GRAY)
-
+smoothen = cv2.GaussianBlur(img_gray,(5,5),0)
 
 
 
