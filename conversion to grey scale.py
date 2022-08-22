0b811eaa3c21c3443c94086d7e6e598991104937
harvis <getharisree@outlook.com>
2022-08-22 11:14:23 +0530

    grey scale conversion

--- conversion to grey scale.py
+++ conversion to grey scale.py
@@ -2,13 +2,16 @@ import cv2
 import numpy as np
 
 img = cv2.imread("test_image.jpg")
-imgcpy = np.copy(img)
-img_gray = cv2.cvtColor(imgcpy,cv2.COLOR_BGR2GRAY)
-smoothen = cv2.GaussianBlur(img_gray,(5,5),0)
+grey = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+blurr = cv2.GaussianBlur(grey,(5,5),0)
+highlight = cv2.Canny(blurr,50,150)
 
 
 
 
-cv2.imshow("image",smoothen)
 
+
+
+
+cv2.imshow("output",highlight)
 cv2.waitKey(0)
