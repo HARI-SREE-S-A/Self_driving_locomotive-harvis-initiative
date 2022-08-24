8572ced7c90c6c88ac02b99d011488a1e9d4db77
harvis <getharisree@outlook.com>
2022-08-24 18:42:29 +0530

    Create region of interest.py

---
+++ region of interest.py
@@ -0,0 +1,31 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def canny(image):
+    grey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    blurr = cv2.GaussianBlur(grey, (5, 5), 0)
+    highlight = cv2.Canny(blurr, 50, 150)
+    return highlight
+def req_reg(image):
+    height = image.shape[0]
+    triangle = np.array([[(200,height),(1100,height),(550,250)]])
+    mask = np.zeros_like(image)
+    cv2.fillPoly(mask,triangle,255)
+    return mask
+
+img = cv2.imread("test_image.jpg")
+cann = canny(img)
+
+
+
+
+
+
+
+
+
+
+cv2.imshow("test",req_reg(img))
+cv2.waitKey(0)
